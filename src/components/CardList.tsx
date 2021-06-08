23da9f62481857378ca5e027c52352244cf66e3b
Lucas Lopes <dev.thuram@gmail.com>
2021-06-08 15:01:09 -0300

    finish challenge

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,24 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // TODO SELECTED IMAGE URL STATE
+  const [currentUrl, setCurrentUrl] = useState('');
 
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  const handleViewImage = (url: string): void => {
+    setCurrentUrl(url);
+    onOpen();
+  };
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing={10}>
+        {cards.map(card => (
+          <Card key={card.id} data={card} viewImage={handleViewImage} />
+        ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage isOpen={isOpen} onClose={onClose} imgUrl={currentUrl} />
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,25 @@ import { Button, Box } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
+import { AxiosResponse } from 'axios';
 import { Header } from '../components/Header';
 import { CardList } from '../components/CardList';
 import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type Image = {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+};
+
 export default function Home(): JSX.Element {
+  const fetchImages = ({ pageParam = 0 }): any =>
+    api(`/images?after=${pageParam}`).then(({ data }) => data);
+
   const {
     data,
     isLoading,
@@ -16,20 +28,25 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastPage => lastPage.after,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    const formated = [];
+
+    data?.pages.map(dataImage => formated.push(...dataImage.data));
+
+    return formated;
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    <Error />;
+  }
 
   return (
     <>
@@ -37,7 +54,12 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage}>
+            {isFetchingNextPage ? 'Carregando aplicação...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
